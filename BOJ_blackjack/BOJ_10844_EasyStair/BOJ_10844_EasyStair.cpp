0cdb9108d540ee839031ad201ef62a1d3b1f79e4
Taemin11 <haruto4380@gmail.com>
2019-07-24 16:46:14 +0900

    0724 boj study

---
+++ BOJ_blackjack/BOJ_10844_EasyStair/BOJ_10844_EasyStair.cpp
@@ -0,0 +1,30 @@
+﻿//쉬운 계단 수
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int dp[101][11];
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	for (int i = 1; i < 10; i++) {
+		dp[1][i] = 1;
+	}
+
+	for (int i = 2; i <= n; i++) {
+		dp[i][0] = dp[i - 1][1];
+		for (int j = 1; j <= 9; j++) {
+			dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
+		}
+
+	}
+	long sum = 0;
+	for (int i = 0; i < 10; i++) {
+		sum += dp[n][i];
+	}
+
+	cout << sum%1000000000;
+}
---
+++ BOJ_blackjack/BOJ_1463/BOJ_1463.cpp
@@ -0,0 +1,52 @@
+﻿//1로 만들기
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int dp[1000001];
+
+int one(int n) {
+	if (n == 1) {
+		dp[1] = 0;
+		return 0;
+	}
+	else if (n == 2) {
+		dp[2] = 1;
+		return 1;
+	}
+	else if (n == 3) {
+		dp[3] = 1;
+		return 1;
+	}
+	else if (dp[n] != 0) {
+		return dp[n];
+	}
+	else {
+		int s1 = n - 1;
+		int temp = 1000001;
+		if (n % 2 == 0) {
+			int s2 = n / 2;
+			temp = min(temp, one(s2));
+		}
+
+		if (n % 3 == 0) {
+			int s3 = n / 3;
+			temp = min(temp, one(s3));
+		}
+
+		temp = min(temp, one(s1));
+	
+		return dp[n] = temp + 1;
+	}
+}
+
+
+int main() {
+	dp[0] = 0;
+	int n;
+	cin >> n;
+	one(n);
+	cout << dp[n];
+
+	
+}
