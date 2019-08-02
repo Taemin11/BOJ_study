d0b16733142eeb9331ae9a4d58bd76e6a314375b
Taemin11 <haruto4380@gmail.com>
2019-08-02 17:18:05 +0900

    0802 boj study

---
+++ BOJ_blackjack/BOJ_1712_breakEvenPoint/BOJ_1712_breakEvenPoint.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+vector<int> v;
+int main() {
+	int a;
+	cin >> a;
+	int b;
+	cin >> b;
+	int c;
+	cin >> c;
+	int benefit = c - b;
+	int k = 0;
+
+	if (benefit <= 0) {
+		cout << -1;
+		return 0;
+	}
+
+	k = a / benefit + 1;
+	
+
+	cout << k;
+	return 0;
+}
---
+++ BOJ_blackjack/BOJ_SugerDelivery/BOJ_2839_SugerDelivery.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+vector<int> v;
+
+int main() {
+	int n;
+	cin >> n;
+	int ans = 0;
+	while (1) {
+		if (n % 5 == 0) {
+			ans += n / 5;
+			cout << ans;
+			break;
+		}
+
+		n = n - 3;
+		ans++;
+
+		if (n < 0) {
+			cout << -1;
+			break;
+		}
+	}
+
+	
+
+}
