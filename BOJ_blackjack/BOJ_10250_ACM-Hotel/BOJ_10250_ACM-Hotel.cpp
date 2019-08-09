a8f093b6a90a59572388bb9e2b89a479fda49395
Taemin11 <haruto4380@gmail.com>
2019-08-09 17:17:16 +0900

    0806 boj study

---
+++ BOJ_blackjack/BOJ_10250_ACM-Hotel/BOJ_10250_ACM-Hotel.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		int h, w, n;
+		cin >> h >> w >> n;
+		int f = n % h;
+		int s = n / h;
+		if (n % h == 0) {
+			f = h;
+			s = s - 1;
+		}		
+		if (n < h * 9) {
+			cout << f << 0 << s + 1;
+		}
+		else {
+			cout << f << s + 1;
+		}
+
+		cout << " ";
+	}
+
+
+}
---
+++ BOJ_blackjack/BOJ_2869_SnailWantToClimb/BOJ_2869_SnailWantToClimb.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+
+	int a, b, v;
+	cin >> a >> b >> v;
+	int daycount = a - b;
+	if ((v - a) % daycount == 0) {
+		cout << (v - a) / daycount + 1;
+	}
+	else {
+		cout << (v - a) / daycount + 2;
+	}
+}
